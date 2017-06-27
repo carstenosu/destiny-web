8a4879c88ef8ad8639e621a8c22febbe97ec2489
carstenosu <foley.126@gmail.com>
2017-06-26 19:47:10 -0500

    Oh wow promises are crazy

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const handlers = require('../handlers/user.js');
+const userHandler = require('../handlers/user.js');
 
 const destiny = require('node-destiny');
 const apiKey = process.env.BUNGIE_API_KEY;
@@ -19,7 +19,7 @@ router.post('/user', function(req,res){
 });
 
 router.get('/user/:gamertag/:membershipType/:membershipId/:characterId', function(req, res){
-  const characterSummaryPromise = handlers.getCharacterSummary(req,res).then( response => {
+  const characterSummaryPromise = userHandler.getCharacterSummary(req.params.membershipType, req.params.membershipId, req.params.caharcterId).then( response => {
     res.render('character', { characterSummary: response, title: `Destiny - ${req.params.gamertag}`})
   });
   
@@ -27,63 +27,9 @@ router.get('/user/:gamertag/:membershipType/:membershipId/:characterId', functio
 
 // User landing page
 router.get('/user/:gamertag', function(req, res) {
-
-  let charSummaries = [];
-  const membershipType = '1';
-
-  destinyClient.search(membershipType, req.params.gamertag).then( response => {
-
-    const membership = response.data.Response[0];
-    const membershipId = membership.membershipId;
-
-    destinyClient.getAccountSummary( membershipType, membershipId ).then( response => {
-
-      console.log('Got Membership Summary');
-        const characters = response.data.Response.data.characters;
-        const grimoireScore = 4615;
-
-        destinyClient.definitions = true;
-
-        const characterId1 = characters[0].characterBase.characterId;
-        const characterId2 = characters[1].characterBase.characterId;
-        const characterId3 = characters[2].characterBase.characterId;
-
-        const params = { mode: 'Raid', count: '10', page: '0'};
-
-        const characterSummary1Promise = destinyClient.getCharacterSummary( membershipType, membershipId, characterId1 );
-        const characterSummary2Promise = destinyClient.getCharacterSummary( membershipType, membershipId, characterId2 );
-        const characterSummary3Promise = destinyClient.getCharacterSummary( membershipType, membershipId, characterId3 );
-
-        Promise.all( [characterSummary1Promise, characterSummary2Promise, characterSummary3Promise]).then( characterSummaries => {
-            
-            characterSummaries.forEach( characterSummary => {
-                const characterResponse = characterSummary.data.Response.data
-                const definitions = characterSummary.data.Response.definitions;
-
-                const genderHash = characterResponse.characterBase.genderHash;
-                const raceHash = characterResponse.characterBase.raceHash;
-                const classHash = characterResponse.characterBase.classHash;
-
-                const genderName = definitions.genders[genderHash].genderName;
-                const raceName = definitions.races[raceHash].raceName;
-                const className = definitions.classes[classHash].className;
-
-                characterResponse.characterBase.genderName = genderName;
-                characterResponse.characterBase.raceName = raceName;
-                characterResponse.characterBase.className = className;
-
-                characterResponse.membershipType = membershipType;
-                characterResponse.membershipId = membershipId;
-                characterResponse.gamertag = req.params.gamertag;
-                charSummaries.push(characterResponse);
-            })
-            
-            
-            res.render('user', { characterSummaries: charSummaries, title: `Destiny - ${req.params.gamertag}`, message: `${req.params.gamertag}` })
-        })
-    })
+  const userPromise = userHandler.loadUser(req.params.gamertag).then( response => {
+    res.render('user', { characterSummaries: response, title: `Destiny - ${req.params.gamertag}`, message: `${req.params.gamertag}` })
   })
-
 })
 
 module.exports = router
--- handlers/user.js
+++ handlers/user.js
@@ -3,12 +3,66 @@ const apiKey = process.env.BUNGIE_API_KEY;
 
 const destinyClient = new destiny.DestinyClient(apiKey, 'https://www.bungie.net/platform/Destiny');
 
-function getCharacterSummary( req, res ) {
+function loadUser( gamertag ) {
+    let charSummaries = [];
+    const membershipType = '1';
+    let membershipId = null;
+    let characterIds = [];
+    let characterPromises = [];
+
+    return destinyClient.search(membershipType, gamertag).then( response => {
+        const membership = response.data.Response[0];
+        membershipId = membership.membershipId;
+    }).then( response => {
+        return destinyClient.getAccountSummary( membershipType, membershipId ).then( response => {
+            console.log('Got Membership Summary');
+            const characters = response.data.Response.data.characters;
+            const grimoireScore = 4615;
+
+            destinyClient.definitions = true;
+
+            for ( var tempChar of characters ) {
+                characterIds.push(tempChar.characterBase.characterId);
+                characterPromises.push(destinyClient.getCharacterSummary( membershipType, membershipId, tempChar.characterBase.characterId ));        
+            }
+        })
+    }).then( response => {
+        const params = { mode: 'Raid', count: '10', page: '0'};
+        return Promise.all( characterPromises ).then( characterSummaries => {
+            characterSummaries.forEach( characterSummary => {
+                    const characterResponse = characterSummary.data.Response.data
+                    const definitions = characterSummary.data.Response.definitions;
+
+                    const genderHash = characterResponse.characterBase.genderHash;
+                    const raceHash = characterResponse.characterBase.raceHash;
+                    const classHash = characterResponse.characterBase.classHash;
+
+                    const genderName = definitions.genders[genderHash].genderName;
+                    const raceName = definitions.races[raceHash].raceName;
+                    const className = definitions.classes[classHash].className;
+
+                    characterResponse.characterBase.genderName = genderName;
+                    characterResponse.characterBase.raceName = raceName;
+                    characterResponse.characterBase.className = className;
+
+                    characterResponse.membershipType = membershipType;
+                    characterResponse.membershipId = membershipId;
+                    characterResponse.gamertag = gamertag;
+                    charSummaries.push(characterResponse);
+                })
+                return charSummaries;
+        })
+        
+    })
+
+}
+
+function getCharacterSummary( membershipType, membershipId, characterId ) {
     console.log('hander for getcharacter summary');
 
     destinyClient.definitions = true;
 
-    return destinyClient.getCharacterSummary(req.params.membershipType, req.params.membershipId, req.params.characterId).then( response => {
+    return destinyClient.getCharacterSummary(membershipType, membershipId, characterId).then( response => {
         const characterSummary = response.data.Response.data;
         const definitions = response.data.Response.definitions;
         const genderHash = characterSummary.characterBase.genderHash;
@@ -30,4 +84,13 @@ function getCharacterSummary( req, res ) {
     })
 }
 
+function getRaidHistory( membershipType, membershipId, characterId ) {
+    const params = { mode: 'Raid', count: '20', page: '0'};
+    return destinyClient.getActivityHistory(membershipType, membershipId, characterId, params).then( response => {
+        return response;
+    });
+}
+
+module.exports.loadUser = loadUser;
 module.exports.getCharacterSummary = getCharacterSummary;
+module.exports.getRaidHistory = getRaidHistory;
